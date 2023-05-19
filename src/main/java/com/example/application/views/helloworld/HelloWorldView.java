f06576dc88b0d1f14b292cbe2c1caf64144f917a
edigonzales <edi.gonzales@gmail.com>
2023-05-19 19:01:27 +0200

    wip

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -6,6 +6,9 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.FileBuffer;
+import com.vaadin.flow.component.upload.receivers.FileData;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
@@ -26,10 +29,42 @@ public class HelloWorldView extends HorizontalLayout {
         });
         sayHello.addClickShortcut(Key.ENTER);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        FileBuffer fileBuffer = new FileBuffer();
+        Upload singleFileUpload = new Upload(fileBuffer);
 
-        add(name, sayHello);
+        singleFileUpload.addSucceededListener(event -> {
+            // Get information about the file that was written to the file
+            // system
+            FileData savedFileData = fileBuffer.getFileData();
+            String absolutePath = savedFileData.getFile().getAbsolutePath();
+
+            System.out.printf(savedFileData.getFileName());
+            System.out.printf("File saved to: %s%n", absolutePath);
+            
+            // Hier sollte ich eine Job-ID zurückbekommen.
+            // Mit dieser Frage ich im FinishedListener nach.
+            
+            
+//            for (int i=0; i<20; i++) {
+//                try {
+//                    System.out.println("do something...");
+//                    Thread.sleep(2000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+        });
+        
+        singleFileUpload.addFinishedListener(event -> {
+            System.out.println(event.getMIMEType());
+        });
+
+        
+        
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello, singleFileUpload);
+
+        add(name, sayHello, singleFileUpload);
     }
 
 }
